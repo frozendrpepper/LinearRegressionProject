a778323f51332dd9daf59895c7e8b477d47668c5
frozendrpepper <ckang9000@gmail.com>
2017-10-16 16:32:51 +0900

    minor change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created on Tue Oct  3 00:52:59 2017
 from sklearn.cross_validation import KFold, StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
-
+from sklearn.preprocessing import Imputer
 
 import numpy as np
 import pandas as pd
@@ -18,27 +18,21 @@ def apply_imputer(style):
 
 '''Import the train and test data set'''
 data_train = pd.read_csv('train.csv')
-data_test = pd.read_csv('test.csv')
 
 X = data_train.iloc[:, :291]
 y = data_train.iloc[:, 291]
-'''Cut out id/timestamp and separate the data into categorical and numerical
-data'''
+'''Cut out id/timestamp and separate the data into categorical and numerical data'''
 id_timestamp_data = data_train.loc[:, ['id', 'timestamp']]
 
 '''Categorical data preprocessing'''
-categorical_list =['product_type', 'sub_area', 'thermal_power_plant_raion', 'incineration_raion',
+categorical_list =['material', 'state', 'product_type', 'sub_area', 'thermal_power_plant_raion', 'incineration_raion',
                    'oil_chemistry_raion', 'radiation_raion', 'railroad_terminal_raion', 'big_market_raion',
                    'nuclear_reactor_raion', 'detention_facility_raion', 'ID_metro', 'ID_railroad_station_walk',
                    'ID_railroad_station_avto', 'water_1line', 'ID_big_road1', 'big_road1_1line',
                    'ID_big_road2', 'railroad_1line', 'ID_railroad_terminal', 'ID_bus_terminal', 'ecology']
 categorical_data = data_train.loc[:,categorical_list]
 
-'''Extract unique elements in each category column so we can use it to
-apply imputer class'''
-categorical_data['product_type'].unique()
-#numerical_list = []
-#numerical_data = data_train.loc[:, ]
+
 
 '''From the original feature matrix, get rid of the categorical columns and 
 columns that are unnecessary'''
